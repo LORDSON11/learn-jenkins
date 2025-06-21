d4130ef194435381b912d3b6342ff8b41428f814
LORDSON11 <lordsondavid.ei21@bitsathy.ac.in>
2025-06-21 15:17:39 +0530

    Fixed ami  issues

--- terraform-docker/terraform/main.tf
+++ terraform-docker/terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "docker_ec2" {
-  ami           = "ami-0e001c9271cf7f3b9"
+  ami           = "ami-0c768662cc797cd75"
   instance_type = "t2.micro"
   key_name      = "node"
 
